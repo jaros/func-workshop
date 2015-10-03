b07b42a15741f24db48581443433e0a9bdb260b6
jaros <judin.jaroslav@gmail.com>
2015-10-03 13:39:50 +0200

    filter - 3nd iteration - remove optional semicolons

--- src/main/groovy/net/sample/Filter.groovy
+++ src/main/groovy/net/sample/Filter.groovy
@@ -6,21 +6,21 @@ class Filter {
 
     public static void main(String[] args) {
 
-        List<Integer> input = new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3));
+        List<Integer> input = new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))
 
 
-        ArrayList<Integer> result = filterEven(input);
-        println result;
+        ArrayList<Integer> result = filterEven(input)
+        println result
     }
 
     private static ArrayList<Integer> filterEven(ArrayList<Integer> input) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> result = new ArrayList<>()
         for (Integer number : input) {
             if (number % 2 == 0) {
-                result.add(number);
+                result.add(number)
             }
         }
-        result;
+        result
     }
 
 }
