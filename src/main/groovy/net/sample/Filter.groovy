ab82048672665e71947eec19d6b69f613c2ce755
jaros <judin.jaroslav@gmail.com>
2015-10-03 14:00:03 +0200

    filter - 8th iteration - use implicit parameter inside each closure

--- src/main/groovy/net/sample/Filter.groovy
+++ src/main/groovy/net/sample/Filter.groovy
@@ -15,9 +15,9 @@ class Filter {
 
     private static List<Integer> filterEven(List<Integer> input) {
         List<Integer> result = new ArrayList<>()
-        input.each { number ->
-            if (number % 2 == 0) {
-                result.add(number)
+        input.each {
+            if (it % 2 == 0) {
+                result.add(it)
             }
         }
         result
