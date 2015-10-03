adfceab97114654b713ab772b22548f917f9942e
jaros <judin.jaroslav@gmail.com>
2015-10-03 13:46:09 +0200

    filter - 4nd iteration - input parameter is immutable

--- src/main/groovy/net/sample/Filter.groovy
+++ src/main/groovy/net/sample/Filter.groovy
@@ -6,14 +6,14 @@ class Filter {
 
     public static void main(String[] args) {
 
-        List<Integer> input = new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))
+        List<Integer> input = Arrays.asList(-3, -2, -1, 0, 1, 2, 3)
 
 
         ArrayList<Integer> result = filterEven(input)
         println result
     }
 
-    private static ArrayList<Integer> filterEven(ArrayList<Integer> input) {
+    private static ArrayList<Integer> filterEven(List<Integer> input) {
         List<Integer> result = new ArrayList<>()
         for (Integer number : input) {
             if (number % 2 == 0) {
