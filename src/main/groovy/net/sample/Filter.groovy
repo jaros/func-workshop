79d6848535f2f7816f4c58fb68253546ac058cfe
jaros <judin.jaroslav@gmail.com>
2015-10-03 13:26:53 +0200

    add filter sample

---
+++ src/main/groovy/net/sample/Filter.groovy
@@ -0,0 +1,15 @@
+package net.sample
+
+
+class Filter {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> input = new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3));
+
+
+        println input
+    }
+
+}
