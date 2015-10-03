036f38bb9b1d82deb7df63a870f2218925864a90
jaros <judin.jaroslav@gmail.com>
2015-10-03 15:44:13 +0200

    fold 2 - replace main method with script

--- src/main/groovy/net/sample/Fold.groovy
+++ src/main/groovy/net/sample/Fold.groovy
@@ -1,16 +1,11 @@
 package net.sample
 
 
-public class Fold {
-    public static void main(String[] args) {
+List<Integer> numbers = Arrays.asList(3, 7, 5, 5, 100);
+int sum = 0;
 
-        List<Integer> numbers = Arrays.asList(3, 7, 5, 5, 100);
-        int sum = 0;
-
-        for (Integer num : numbers) {
-            sum = sum + num;
-        }
-
-        println sum;
-    }
+for (Integer num : numbers) {
+    sum = sum + num;
 }
+
+println sum;
