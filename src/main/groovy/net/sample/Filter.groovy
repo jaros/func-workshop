16a0321f83e76ffb09812ccdce9ed26e5e12e0ac
jaros <judin.jaroslav@gmail.com>
2015-10-03 14:32:45 +0200

    filter - 17th iteration - switch from Main to groovy script

--- src/main/groovy/net/sample/Filter.groovy
+++ src/main/groovy/net/sample/Filter.groovy
@@ -1,15 +1,20 @@
 package net.sample
 
+import org.codehaus.groovy.runtime.InvokerHelper
 
-class Filter {
 
+class Filter extends Script {
 
-    static void main(String[] args) {
-
+    @Override
+    def run() {
         def input = [-3, -2, -1, 0, 1, 2, 3]
 
         println input.grep { it % 2 == 0 }
     }
 
+    static void main(String[] args) {
+        InvokerHelper.runScript(Filter, args)
+
+    }
 
 }
