605f26c525fd498fd62a863748b70325b882aebd
jaros <judin.jaroslav@gmail.com>
2015-10-03 14:33:30 +0200

    filter - 18th iteration - simplify script syntax - it's injected by default

--- src/main/groovy/net/sample/Filter.groovy
+++ src/main/groovy/net/sample/Filter.groovy
@@ -1,20 +1,5 @@
 package net.sample
 
-import org.codehaus.groovy.runtime.InvokerHelper
+def input = [-3, -2, -1, 0, 1, 2, 3]
 
-
-class Filter extends Script {
-
-    @Override
-    def run() {
-        def input = [-3, -2, -1, 0, 1, 2, 3]
-
-        println input.grep { it % 2 == 0 }
-    }
-
-    static void main(String[] args) {
-        InvokerHelper.runScript(Filter, args)
-
-    }
-
-}
+println input.grep { it % 2 == 0 }
