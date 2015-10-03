95dfc7508af5dbfd58558ac2873513b5babc0b21
jaros <judin.jaroslav@gmail.com>
2015-10-03 14:05:35 +0200

    filter - 9th iteration - replace each with grep function
    groovy collection api method

--- src/main/groovy/net/sample/Filter.groovy
+++ src/main/groovy/net/sample/Filter.groovy
@@ -14,13 +14,7 @@ class Filter {
     }
 
     private static List<Integer> filterEven(List<Integer> input) {
-        List<Integer> result = new ArrayList<>()
-        input.each {
-            if (it % 2 == 0) {
-                result.add(it)
-            }
-        }
-        result
+        return input.grep({ it % 2 == 0 })
     }
 
 }
