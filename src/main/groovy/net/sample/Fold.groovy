216326cbd7f0370b814d19e29c4442362730c994
jaros <judin.jaroslav@gmail.com>
2015-10-04 11:01:51 +0200

    fold 7 - show custom functional impl for folding

--- src/main/groovy/net/sample/Fold.groovy
+++ src/main/groovy/net/sample/Fold.groovy
@@ -3,6 +3,12 @@ package net.sample
 
 def numbers = [3, 7, 5, 5, 100]
 
-int sum = numbers.inject{ a, b -> a + b }
+int sum = findSum(0, numbers)
+
+int findSum(Integer acc, List<Integer> nums) {
+    if (nums == null || nums.empty) {
+        acc
+    } else findSum(acc + nums.pop(), nums)
+}
 
 println sum
